91401575613d6b751dd41954278a6097c2e9edc7
NeuralNacho <anguswhittam@btinternet.com>
2023-03-25 15:41:50 +0000

    Sync desktop with laptop Q8

--- Q8_displayresults.R
+++ Q8_displayresults.R
@@ -6,7 +6,8 @@ Q8_displayresults <- function() {
   lars_cv_error <- c()
   Q5_greedy_end_error <- c() 
   # ^ will be error from final column of Q5_greedysubset
-  for (i in 1:30) {
+  for (i in 1:100) {
+    print(i)
     errors <- Q8_getresults()
     # All of the test data RSS in a list
     bestsubset_error <- errors[[1]]  # USELESS BIASED
@@ -17,6 +18,18 @@ Q8_displayresults <- function() {
     lars_cv_error[i] <- errors[[6]]
     Q5_greedy_end_error[i] <- tail(errors[[3]], n = 1)
   }
-  boxplot(list(bestsubset_cv_error, greedysubset_cv_error, 
-               lars_cv_error, Q5_greedy_end_error))
+  data <- data.frame(Bestsubset_Crossval = bestsubset_cv_error,
+               Greedysubset_Crossval = greedysubset_cv_error,
+               LARS_Crossval = lars_cv_error,
+               Q5_Greedysubset = Q5_greedy_end_error)
+  means <- colMeans(data)
+  boxplot(data,
+          xlab = 'Variable Selection Method',
+          ylab = 'Test Error',
+          col=c("indianred1","skyblue",
+                "palegreen","lightgoldenrod"))
+  points(x = 1:4,  # Add mean points to plot
+         y = means,
+         col = "black",
+         pch = 16)
 }
